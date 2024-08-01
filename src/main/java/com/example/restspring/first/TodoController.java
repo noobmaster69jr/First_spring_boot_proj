413bd4e071d36debaa0434f76c0c7520952e8b4f
noobmaster69jr <srashwin22@gmail.com>
2024-08-01 10:29:06 +0530

    some edit

--- src/main/java/com/example/restspring/first/TodoController.java
+++ src/main/java/com/example/restspring/first/TodoController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
+@RequestMapping("/api/v1")
 public class TodoController {
   private static List<Todo> todoList;
   // Error message when the todo is not found
