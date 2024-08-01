b28e2878f06d208ade85c0325aac328515232a29
noobmaster69jr <srashwin22@gmail.com>
2024-08-01 09:44:00 +0530

    added response entity to send response code

--- src/main/java/com/example/restspring/first/TodoController.java
+++ src/main/java/com/example/restspring/first/TodoController.java
@@ -3,6 +3,8 @@ package com.example.restspring.first;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,24 +13,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class TodoController {
-  private static List<Todo> todos;
+  private static List<Todo> todoList;
   
   public TodoController(){
-    todos = new ArrayList<>();
-     todos.add(new Todo(1, false, "Todo 1", 1));
-    todos.add(new Todo(2, true, "Todo 2", 2));
+    todoList = new ArrayList<>();
+     todoList.add(new Todo(1, false, "Todo 1", 1));
+    todoList.add(new Todo(2, true, "Todo 2", 2));
   }
 
 
-  @GetMapping("/todos") 
-  public List<Todo> getTodos(){
-    return todos;
-  }  
+   @GetMapping("/todos")
+    public ResponseEntity<List<Todo>> getTodos() {
+        return ResponseEntity.ok(todoList);
+    }
 
-   @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo newTodo) {
-        todos.add(newTodo);
-        return newTodo;
+    @PostMapping("/todos")
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
+
+        /**
+         * we can use this annotation to set the status code @ResponseStatus(HttpStatus.CREATED)
+         *
+         */
+        todoList.add(newTodo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
   
 }
