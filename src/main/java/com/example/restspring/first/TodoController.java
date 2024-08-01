e692f167cbbf5fd5683e166e9ab76190fa694f7c
noobmaster69jr <srashwin22@gmail.com>
2024-08-01 09:40:08 +0530

    get and post

--- src/main/java/com/example/restspring/first/TodoController.java
+++ src/main/java/com/example/restspring/first/TodoController.java
@@ -1,7 +1,13 @@
+package com.example.restspring.first;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 public class TodoController {
@@ -11,6 +17,18 @@ public class TodoController {
     todos = new ArrayList<>();
      todos.add(new Todo(1, false, "Todo 1", 1));
     todos.add(new Todo(2, true, "Todo 2", 2));
-    
   }
+
+
+  @GetMapping("/todos") 
+  public List<Todo> getTodos(){
+    return todos;
+  }  
+
+   @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo) {
+        todos.add(newTodo);
+        return newTodo;
+    }
+  
 }
